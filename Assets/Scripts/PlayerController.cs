d5ddd3fe5e44c531ca3712d574c62245c99c8cb6
Joonas Erho <joonas.erho@gmail.com>
2025-06-14 20:30:47 +0300

    added gameplay elements, fixed player movement, added main menu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,24 +12,50 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private Rigidbody2D _rb;
 
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Transform _tf;
+
+    [SerializeField]
+    private Tilemap _tilemap;
+
+    [SerializeField]
+    private LevelController _levelController;
 
     void Update()
     {
+        // Move actual movement to FixedUpdate
         if (Input.GetKey(KeyCode.A))
         {
-            _rb.AddForceX(_verticalSpeed * -0.1f);
+            _tf.position = new Vector2(_tf.position.x - _verticalSpeed, _tf.position.y);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            _rb.AddForceX(_verticalSpeed * 0.1f);
+            _tf.position = new Vector2(_tf.position.x + _verticalSpeed, _tf.position.y);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _rb.AddForceY(_jumpSpeed * 0.1f);
+        }
+
+        Vector3 playerWorldPos = transform.position;
+        Vector3Int cellPos = _tilemap.WorldToCell(playerWorldPos);
+
+        TileBase tile = _tilemap.GetTile(cellPos);
+
+        if (tile is CustomPropertyTile customPropertyTile)
+        {
+            if (customPropertyTile.type == TileType.GOAL)
+            {
+                Debug.Log("Player stepped on the goal! Level complete!");
+            }
+            else if (customPropertyTile.type == TileType.SCOREITEM)
+            {
+                _levelController.CollectWisp(cellPos);
+            }
+            else if (customPropertyTile.type == TileType.ADDITIONALMIRROR)
+            {
+                _levelController.CollectMirror(cellPos);
+            }
         }
     }
 }
